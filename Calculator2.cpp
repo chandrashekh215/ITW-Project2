f3a246715499d1aa94180554eb444138388cce7e
bhavya <152707659+bhavyasri-24@users.noreply.github.com>
2024-11-13 21:27:58 +0530

    added power and percentage functionalities

---
+++ Calculator2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+  char operation;
+  double num1, num2;
+
+
+  cout << "Calculator\n";
+  cout << "Choose an operation:\n";
+  cout << "+ for addition\n- for subtraction\n* for multiplication\n/ for division\n";
+  cout << "% for modulus (integers only)\n^ for power\np for percentage\n";
+  cout << "Enter operation : ";
+  cin >> operation;
+
+
+  cout << "Enter two numbers: ";
+  cin >> num1 >> num2;
+
+
+  switch (operation)
+  {
+  case '+':
+    cout << "Result: " << num1 + num2 << endl;
+    break;
+  case '-':
+    cout << "Result: " << num1 - num2 << endl;
+    break;
+  case '*':
+    cout << "Result: " << num1 * num2 << endl;
+    break;
+  case '/':
+    if (num2 != 0)
+      cout << "Result: " << num1 / num2 << endl;
+    else
+      cout << "Error: Division by zero is not allowed.\n";
+    break;
+  case '%':
+    if ((int)num2 != 0)
+      cout << "Result: " << (int)num1 % (int)num2 << endl;
+    else
+      cout << "Error: Modulus by zero is not allowed.\n";
+    break;
+  case '^':
+    cout << "Result: " << pow(num1, num2) << endl;
+    break;
+  case 'p':
+    cout << "Result: " << (num1 * num2 / 100) << " (as " << num2 << "% of " << num1 << ")\n";
+    break;
+  default:
+    cout << "Invalid operation!\n";
+    break;
+  }
+
+
+  return 0;
+}
