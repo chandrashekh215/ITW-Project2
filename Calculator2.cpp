0c7fe4169879271183e789c3fb424afb29349380
PR-Skandha-B <skandha.bharadwaj.75@mail.com>
2024-11-14 14:36:22 +0530

    Added Logrithm with base 10, Sine, Cosine,Tangent

--- Calculator2.cpp
+++ Calculator2.cpp
@@ -13,13 +13,37 @@ int main()
   cout << "Choose an operation:\n";
   cout << "+ for addition\n- for subtraction\n* for multiplication\n/ for division\n";
   cout << "% for modulus (integers only)\n^ for power\np for percentage\n";
+  cout <<  "l for natural logarithm\nL for log base 10\ns for sine\nc for cosine\nt for tangent\n";
   cout << "Enter operation : ";
   cin >> operation;
+  if ( operation == 'L' ||operation == 's' || operation == 'c' || operation == 't' || ) {
+        cout << "Enter a number: ";
+        cin >> num1;
+		switch (operation)
+  {
+  case 'L':
+    if (num1 > 0) cout << "Result: " << log10(num1) << endl;
+    else cout << "Error: Logarithm of non-positive number is undefined.\n";
+    break;
+  case 's':
+    cout << "Result: " << sin(num1) << endl;
+    break;
+  case 'c':
+    cout << "Result: " << cos(num1) << endl;
+    break;
+  case 't':
+    cout << "Result: " << tan(num1) << endl;
+    break;    
+  default:
+    cout << "Invalid operation!\n";
+    break;
+  }
+  
+  }
 
-
+  else{
   cout << "Enter two numbers: ";
-  cin >> num1 >> num2;
-
+  cin >> num1 >> num2;}
 
   switch (operation)
   {
@@ -49,12 +73,10 @@ int main()
     break;
   case 'p':
     cout << "Result: " << (num1 * num2 / 100) << " (as " << num2 << "% of " << num1 << ")\n";
-    break;
+    break;    
   default:
     cout << "Invalid operation!\n";
     break;
   }
-
-
   return 0;
 }
